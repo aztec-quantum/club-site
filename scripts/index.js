761c87e3541fc362640d58bf3527b2d0de4c7624
enochchigbu <enochchigbu@gmail.com>
2023-10-01 18:23:30 -0700

    Add carousel for about page

--- scripts/index.js
+++ scripts/index.js
@@ -20,3 +20,35 @@ var anchorLinks = document.getElementsByClassName("nav-selector");
 window.onload = function(){
     switchPage('home');
 }
+
+// Handle about change slide carousel
+document.addEventListener("DOMContentLoaded", function () {
+    const carouselContainer = document.querySelector(".about-carousel");
+    const slides = document.querySelectorAll(".about-carousel-slide");
+
+    let currentIndex = 0;
+
+    function showSlide(index) {
+        slides.forEach((slide, i) => {
+            slide.style.transform = `translateX(-${index * 100}%)`;
+        });
+    }
+
+    function nextSlide() {
+        currentIndex = (currentIndex + 1) % slides.length;
+        showSlide(currentIndex);
+    }
+
+    function prevSlide() {
+        currentIndex = (currentIndex - 1 + slides.length) % slides.length;
+        showSlide(currentIndex);
+    }
+
+    // Set up event listeners for next and previous buttons
+    document.getElementById("prevBtn").addEventListener("click", prevSlide);
+    document.getElementById("nextBtn").addEventListener("click", nextSlide);
+
+    // Optional: Add automatic slide change
+    // setInterval(nextSlide, 3000); // Change slide every 3 seconds
+});
+
